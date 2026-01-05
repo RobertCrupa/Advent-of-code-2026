0b429c1f862df7424a1f2d51a3edc320970ef664
Robert-George Crupa <robertcrupa@outlook.com>
2026-01-05 17:52:23 +0000

    Solved day 2 part 1

---
+++ Day2/problem.py
@@ -0,0 +1,74 @@
+"""
+The ranges are separated by commas (,); each range gives its first ID and last ID separated by a dash (-).
+
+Since the young Elf was just doing silly patterns, you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice. So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+
+None of the numbers have leading zeroes; 0101 isn't an ID at all. (101 is a valid ID that you would ignore.)
+
+Your job is to find all of the invalid IDs that appear in the given ranges. In the above example:
+
+11-22 has two invalid IDs, 11 and 22.
+95-115 has one invalid ID, 99.
+998-1012 has one invalid ID, 1010.
+1188511880-1188511890 has one invalid ID, 1188511885.
+222220-222224 has one invalid ID, 222222.
+1698522-1698528 contains no invalid IDs.
+446443-446449 has one invalid ID, 446446.
+38593856-38593862 has one invalid ID, 38593859.
+The rest of the ranges contain no invalid IDs.
+Adding up all the invalid IDs in this example produces 1227775554.
+"""
+import math
+
+class process():
+    def __init__(self) -> None:
+        self.sum = 0
+
+    def count_digit(self, num: int):
+        res = 0
+        while num > 0:
+            res += 1
+            num = math.floor(num / 10)
+        return res
+    
+    def is_silly(self, count, num) -> bool:
+        compare = num % (10 ** count)
+        print(f"is silly {num} count {count}")
+
+        for _ in range(count):
+            num //= 10
+
+        print(f"num {num}, compare {compare}")
+
+        return compare == num
+
+
+    def process(self,left: int, right: int):
+        for i in range(left, right):
+            count = self.count_digit(i)
+            if count % 2 != 0:
+                continue
+
+            if self.is_silly(int(count / 2), i):
+                self.sum += i
+                print("summing ", i)
+            
+
+
+
+with open("Day2/example.txt", 'r') as f:
+    solver = process()
+
+    for line in f:
+        print(line)
+        ranges = [x.strip() for x in line.split(',')]
+        print(ranges)
+        nums = [num.split('-') for num in ranges]
+        print(nums)
+        
+        for num in nums:
+            
+            if len(num) == 2:
+                solver.process(int(num[0]), int(num[1]))
+
+    print("solution: ", solver.sum)
